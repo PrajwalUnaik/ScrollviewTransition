e68105224c2a37b146a9d86ce0d5c13789da72fe
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2024-02-03 08:25:02 +0530

    Initial Commit

---
+++ ScrollviewTransition/ScrollviewTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollviewTransitionApp.swift
+//  ScrollviewTransition
+//
+//  Created by Prajwal U on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollviewTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
