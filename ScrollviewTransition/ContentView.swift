42abdcab42ec56239b17d24e6056281d494a773b
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2024-02-03 09:13:47 +0530

    minute changes

--- ScrollviewTransition/ContentView.swift
+++ ScrollviewTransition/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 0) {
+                ForEach(0 ..< 20) { _ in
+                    Color.blue
+                        .cornerRadius(40)
+                        .padding()
+                        .frame(height: UIScreen.main.bounds.height)
+
+                        // MARK: - add this to see animation on scroll view
+
+                        // start (tik-tok transition)
+
+                        .scrollTransition(.interactive, axis:
+                            .vertical) { view, phase in
+                                view.opacity(phase.value < 0 ? 0 : 1.0)
+                                    .offset(x: phase.value
+                                        < 0 ? 500 : 0)
+                                    .blur(radius: phase.value < 0 ? 15 : 0)
+                                    .rotationEffect(.degrees(phase.value < 0 ? -90 : 0))
+                        }
+
+                    // end
+                }
+            }.scrollTargetLayout()
+        }.scrollTargetBehavior(.paging)
+            .ignoresSafeArea()
     }
 }
 
--- ScrollviewTransition/Contentview1.swift
+++ ScrollviewTransition/Contentview1.swift
@@ -1,5 +1,5 @@
 //
-//  Contentview1.swift
+//  ContentView.swift
 //  ScrollviewTransition
 //
 //  Created by Prajwal U on 03/02/24.
@@ -7,12 +7,36 @@
 
 import SwiftUI
 
-struct Contentview1: View {
+struct ContentView1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            VStack {
+                ForEach(0 ..< 20) { _ in
+                    Color.mint
+                        .frame(height: 200)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+
+                        // MARK: - add this to see animation on scroll view
+
+                        // start
+
+                        .scrollTransition(.animated, axis:
+                            .vertical) { view, phase in
+                                view.opacity(phase.value < 0 ? 0.8 : 1.0)
+                                    .offset(x: phase.value
+                                        < 0 ? 500 : 0)
+                                    .blur(radius: phase.value < 0 ? 15 : 0)
+                                    .rotationEffect(.degrees(phase.value < 0 ? -90 : 0))
+                                    .brightness(phase.value < 0 ? 0.8 : 0)
+                        }
+                    // end
+                }
+            }.scrollTargetLayout()
+        }.scrollTargetBehavior(.paging)
     }
 }
 
 #Preview {
-    Contentview1()
+    ContentView1()
 }
